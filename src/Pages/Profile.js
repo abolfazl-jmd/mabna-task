d4bea6d24fb01ecd5a8c3d13fedda02c183d8e74
Abolfazl <abolfazljamshididev@gmail.com>
2022-10-31 22:39:18 +0330

    add tabs component feature

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,10 +1,12 @@
 import ProfilePicture from "../Components/profile-picture-component/ProfilePicture";
+import Tabs from "../Components/tabs-component/Tabs";
 import Layout from "../Layout/Layout";
 
 const Profile = () => {
   return (
     <Layout>
       <ProfilePicture />
+      <Tabs />
     </Layout>
   );
 };
